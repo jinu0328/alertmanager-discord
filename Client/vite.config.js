560e7be014d9fb5d172997ccfa6750766ab9310b
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-08-26 15:01:07 +0900

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,18 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // 외부에서도 접근 가능하게 설정
+    host: '0.0.0.0',  // 외부에서도 접근 가능하게 설정
+    historyApiFallback: true,  // 이 옵션을 추가하여 모든 경로를 index.html로 리디렉션
   },
   build: {
     outDir: 'dist',
   },
   resolve: {
     alias: {
-      '@': '/src', // 필요한 경우 추가
+      '@': '/src',  // 필요한 경우 추가
     },
   },
-  // React Router의 History API 문제 해결
-  server: {
-    historyApiFallback: true, // 이 옵션을 추가하여 모든 경로를 index.html로 리디렉션
-  }
 })
